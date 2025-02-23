4dec3ce1246c1f7979a3a5c98a6ba6954fffafe1
Guillermo Vilanova <guillermovilanova@gmail.com>
2025-02-23 14:34:39 +0100

    revertir

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
-import os
 from flask import Flask
-
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "¡Hola desde Docker en Railway! 🚀"
+    return "¡Hola desde Docker! 🐳"
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # Toma el puerto de Railway
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=5000)
